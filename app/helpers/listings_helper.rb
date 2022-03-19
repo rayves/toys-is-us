9d687c0f4f30c546a0572c2cfa0dd582f02e79e3
rayves <vo.raymond.v@outlook.com>
2022-03-20 02:43:14 +1100

    adds show view

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,15 @@
 module ListingsHelper
+
+    # capitalizes every word in string
+    def format_condition(condition)
+        # condition.gsub("_"," ")
+        # arr = condition.split("_").map do |word|
+        #     word.capitalize
+        # end
+        # arr.join(" ")
+        condition.split("_").map {|word| word.capitalize}.join(" ")
+    end
+    def format_price(price)
+        "$#{price / 100}"
+    end
 end
