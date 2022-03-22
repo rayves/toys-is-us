2dbe43e75870bbeb78235157e0679459ae1a7e03
rayves <vo.raymond.v@outlook.com>
2022-03-22 23:14:02 +1100

    added notes to payments controller explaining actions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,6 +5,7 @@ class PaymentsController < ApplicationController
     @order = Order.find_by(listing_id: params[:id])
   end
 
+# when checkout_session_complete, stripe posts to webhook
   def webhook
     # check with stripe signing secret matches webhook.
       # create event
@@ -12,15 +13,19 @@ class PaymentsController < ApplicationController
         # header
         # endpoint_secret
     begin          
+      # assign the body of the webhook params has payload
     payload = request.raw_post
+      # assign stripe header (signature) - check if header has stripe signiture
     header = request.headers["HTTP_STRIPE_SIGNATURE"]
     secret = Rails.application.credentials.dig(:stripe, :webhook_signature)
+      # check stripe header matches webhook signature in credentials -> if so save event as 'event' variable
     event = Stripe::Webhook.construct_event(payload, header, secret)
     rescue Stripe::SignatureVerificationError => e
-      # error when Stripe tries to check signature -> if invalid signature then error is raised and this error is rescued here.
+      # error when Stripe tries to check signature -> if invalid signature then error is raised and this error is rescued here. Return ending post request before any other line is executed
       render json: {error: "Unauthorised"}, status: 403
       return
     rescue JSON::ParserError => e
+      # rescue if issue with request, return ending post request before any other line is executed
       render json: {error: "Bad Request"}, status: 422
       return
     end
